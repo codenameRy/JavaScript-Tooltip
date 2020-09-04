0141f34dedf7a16d1f78e7c23ab4793551e27ca2
Ryan Lee <ryanm.lee11@gmail.com>
2020-09-03 22:48:38 -0400

    Update Tooltip with hide, fade-in, fade-out, and delay for appearance

--- app.js
+++ app.js
@@ -1,21 +1,62 @@
 let setUpToolTip = function() {
     let tooltip = "",
     toolTipDiv = document.querySelector(".div-tooltip"),
-    toolTipElements = Array.from(document.querySelectorAll(".hover-reveal"));
+    toolTipElements = Array.from(document.querySelectorAll(".hover-reveal")),
+    timer;
 
     let displayToolTip = function(e, obj) {
+        console.log(obj)
         tooltip = obj.dataset.tooltip;
         toolTipDiv.innerHTML = tooltip;
         toolTipDiv.style.top = e.pageY + "px";
         toolTipDiv.style.left = e.pageX + "px";
-        toolTipDiv.style.opacity = 1;
+        // toolTipDiv.style.opacity = 1;
+        fadeIn(toolTipDiv);
+    };
+
+    let fadeOut = function(element) {
+        var op = 1;
+        if (!timer) {
+            timer = setInterval(function() {
+                if (op <= 0.1) {
+                    clearInterval(timer);
+                    timer = null;
+                    element.style.opacity = 0;
+                    element.style.display = 'none';
+                }
+                element.style.opacity = op;
+                op -= op * 0.1;
+            }, 10);
+        }   
+    };
+
+    let fadeIn = function(element) {
+        var op = 0.1;
+        element.style.display = 'block';
+        timer = setInterval(function() {
+            if (op >= 1) {
+                clearInterval(timer);
+            }
+            element.style.opacity = op;
+            op += op * 0.1;
+        }, 10);
     };
 
     toolTipElements.forEach(function(elem) {
+        let timeout;
         elem.addEventListener("mouseenter", function (e) {
-            displayToolTip(e, this);
-        })
-    })
+            that = this;
+            timeout = setTimeout(function () {
+            displayToolTip(e, that);
+            }, 400)
+            
+        });
+        elem.addEventListener("mouseleave", function(e) {
+            // toolTipDiv.style.opacity = 0;
+            clearTimeout(timeout);
+            fadeOut(toolTipDiv);
+        });
+    });
 };
 
 setUpToolTip();

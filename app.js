22dc795a7ce98f47c2c1d7111375cc88ed02bb23
Ryan Lee <ryanm.lee11@gmail.com>
2020-09-04 15:29:56 -0400

    Call and Apply invoke function update

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ let setUpToolTip = function() {
     toolTipElements = Array.from(document.querySelectorAll(".hover-reveal")),
     timer;
 
-    let displayToolTip = function(e, obj) {
-        console.log(obj)
-        tooltip = obj.dataset.tooltip;
+    // let displayToolTip = function(e, obj) {
+        let displayToolTip = function(e) {    
+        // console.log(obj)
+        console.log(this)
+        // tooltip = obj.dataset.tooltip;
+        tooltip = this.dataset.tooltip;
         toolTipDiv.innerHTML = tooltip;
         toolTipDiv.style.top = e.pageY + "px";
         toolTipDiv.style.left = e.pageX + "px";
@@ -42,12 +45,29 @@ let setUpToolTip = function() {
         }, 10);
     };
 
-    toolTipElements.forEach(function(elem) {
+    // toolTipElements.forEach(function(elem) {
+    //     let timeout;
+    //     elem.addEventListener("mouseenter", function (e) {
+    //         // let that = this;
+    //         timeout = setTimeout(function () {
+    //         // displayToolTip(e, that);
+    //         displayToolTip.call(elem, e);
+    //         }, 400)
+            
+    //     });
+    //     elem.addEventListener("mouseleave", function(e) {
+    //         // toolTipDiv.style.opacity = 0;
+    //         clearTimeout(timeout);
+    //         fadeOut(toolTipDiv);
+    //     });
+    // });
+    let bindEvents = function(elem) {
         let timeout;
         elem.addEventListener("mouseenter", function (e) {
-            that = this;
+            // let that = this;
             timeout = setTimeout(function () {
-            displayToolTip(e, that);
+            // displayToolTip(e, that);
+            displayToolTip.call(elem, e);
             }, 400)
             
         });
@@ -56,7 +76,9 @@ let setUpToolTip = function() {
             clearTimeout(timeout);
             fadeOut(toolTipDiv);
         });
-    });
+    }
+
+    toolTipElements.forEach(bindEvents);
 };
 
 setUpToolTip();
